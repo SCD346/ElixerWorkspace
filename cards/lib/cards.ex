46dbe855dc0a129a18f6ecc2bd4b53b2c04d43a0
steve_D <scd346@nyu.edu>
2021-03-27 09:59:36 -0700

    methods added, notes

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,21 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+# Define a method: 1
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
+
+# Define a method: 2
+  # shuffle method found in Enum module of Elixir documentation: https://hexdocs.pm/elixir/Enum.html#member?/2
+    # returns a 'copy', does not edit or impact the original
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+# Define a method: 3
+  # contains method, also found in Enum module
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
