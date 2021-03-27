cb27268958bb9f5b00f88fd5369f0b0c41746796
steve_D <scd346@nyu.edu>
2021-03-27 10:38:52 -0700

    card deck create, 2 solutions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,7 +2,24 @@ defmodule Cards do
 
 # Define a method: 1
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+# SOLUTION 1: WEAK SOLUTION
+  #   cards = for value <- values do
+  #     for suit <- suits do
+  #       "#{value} of #{suit}"
+  #     end
+  #   end
+
+  #   List.flatten(cards)
+  # end
+
+# SOLUTION 2: BETTER SOLUTION
+    # 2 computations running at same time, less code, no additional computations
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
   end
 
 # Define a method: 2
