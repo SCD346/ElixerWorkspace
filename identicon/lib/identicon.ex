257ce67222078c5330deb3230a3c8b429fb682a5
steve_D <scd346@nyu.edu>
2021-03-29 15:18:44 -0700

    get hex vals for RGB with pattern matching

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,14 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    #_tail takes the rest of the hex values away - we dont need them
+    [r, g, b]
   end
 
   # Returns a list of hexcodes
