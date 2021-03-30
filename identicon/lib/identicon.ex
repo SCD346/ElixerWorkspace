090d596dcf6a566fb265bf42368a44ca252cf096
steve_D <scd346@nyu.edu>
2021-03-30 10:01:29 -0700

    build_grid fn added, works

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,13 +3,21 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  # Create grid that determines which squares are colored or white
+    # each inner list represents a row in grid
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
   end
 
 
   # Using pattern matching, get first 3 vals from hex vals,
   # _tail takes the rest of the hex values away - we dont need them
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Indenticon.Image{image | color: {r, g, b}}
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
 
