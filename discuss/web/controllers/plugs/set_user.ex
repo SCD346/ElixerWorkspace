147fbcd33b345e70111cc901def1fe8ca4830d3b
steve_D <scd346@nyu.edu>
2021-04-06 16:39:49 -0700

    user module plug done, needs to be placed in pipeline

--- discuss/web/controllers/plugs/set_user.ex
+++ discuss/web/controllers/plugs/set_user.ex
@@ -2,7 +2,6 @@ defmodule Discuss.Plugs.SetUser do
     import Plug.Conn
     import Phoenix.Controller
 
-    alias Discuss.Router.Helpers
     alias Discuss.Repo
     alias Discuss.User
 
@@ -10,6 +9,13 @@ defmodule Discuss.Plugs.SetUser do
     end
 
     def call(conn, _params) do
+        user_id = get_session(conn, :user_id)
 
+        cond do
+            user_id && Repo.get(User, user_id) ->
+                assign(conn, :user, user)
+            true ->
+                assign(conn, :user, nil)
+        end
     end
 end
