599f66b21847fdb81a5b317d638ba2ba8b1a92fb
steve_D <scd346@nyu.edu>
2021-04-06 16:06:46 -0700

    set user plug set up

---
+++ discuss/web/controllers/plugs/set_user.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.Plugs.SetUser do
+    import Plug.Conn
+    import Phoenix.Controller
+
+    alias Discuss.Router.Helpers
+    alias Discuss.Repo
+    alias Discuss.User
+
+    def init(_params) do
+    end
+
+    def call(conn, _params) do
+
+    end
+end
